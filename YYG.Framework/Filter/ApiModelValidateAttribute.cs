431fc741c85c340aa9a4a6c729ea7831acf7efc1
gangbs <yangyangangil@163.com>
2018-03-10 23:23:35 +0800

    [fix]修改普通模型验证拦截器

--- YYG.Framework/Filter/ApiModelValidateAttribute.cs
+++ YYG.Framework/Filter/ApiModelValidateAttribute.cs
@@ -14,8 +14,25 @@ namespace YYG.Framework.Filter
 {
     public class ApiModelValidateAttribute : ActionFilterAttribute
     {
+        readonly bool canModelNull;
+
+        public ApiModelValidateAttribute(bool canModelNull = false)
+        {
+            this.canModelNull = canModelNull;
+        }
+
+
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
+            string modelName;
+            if (!canModelNull && IsModelNull(actionContext.ActionArguments, out modelName))
+            {
+                var back = new HttpBackResult<string> { Status = (int)HttpBackCode.Parameter, Msg = $"参数\"{modelName}\"不可为空", Data = null };
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.OK);
+                actionContext.Response.Content = new StringContent(back.ToJson());
+                return;
+            }
+
             if (!actionContext.ModelState.IsValid)
             {
                 var back = new HttpBackResult<string> { Status = (int)HttpBackCode.Parameter, Msg = actionContext.ModelState.GetErrorMessage(), Data = null };
@@ -26,6 +43,30 @@ namespace YYG.Framework.Filter
             {
                 base.OnActionExecuting(actionContext);
             }
+        }
+
+        /// <summary>
+        /// 参数模型是否为null
+        /// </summary>
+        /// <param name="actionArguments"></param>
+        /// <returns></returns>
+        private bool IsModelNull(Dictionary<string, object> actionArguments,out string modelName)
+        {
+            modelName = "";
+            bool flag = false;
+            if (actionArguments != null)
+            {
+                foreach (var kv in actionArguments)
+                {
+                    if (kv.Value == null)
+                    {
+                        flag = true;
+                        modelName = kv.Key;
+                        break;
+                    }
+                }
+            }
+            return flag;
         }
     }
 }
