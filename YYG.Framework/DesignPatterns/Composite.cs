841a35098bac6e4168391c176033747fb28b63a8
gangbs <yangyangangil@163.com>
2018-03-13 00:22:24 +0800

    [fix]组合模式树生成方法封装

--- YYG.Framework/DesignPatterns/Composite.cs
+++ YYG.Framework/DesignPatterns/Composite.cs
@@ -24,8 +24,8 @@ namespace YYG.Framework
         private string state { get; set; }
         //public string checked { get; set; }
 
-        public abstract void Add(EasyUITreeNode node);
-        public abstract void Remove(EasyUITreeNode node);
+        public abstract void Add(EasyUITree node);
+        public abstract void Remove(EasyUITree node);
     }
 
     public class EasyUITreeNode : EasyUITree
@@ -36,12 +36,12 @@ namespace YYG.Framework
 
         public List<EasyUITreeNode> children = new List<EasyUITreeNode>();
 
-        public override void Add(EasyUITreeNode node)
+        public override void Add(EasyUITree node)
         {
             children.Add(node);
         }
 
-        public override void Remove(EasyUITreeNode node)
+        public override void Remove(EasyUITree node)
         {
             children.Remove(node);
         }
@@ -49,19 +49,57 @@ namespace YYG.Framework
 
     public class EasyUITreeLeaf : EasyUITree
     {
-        public EasyUITreeLeaf(string id, string text, string state) : base(id, text, state)
+        public EasyUITreeLeaf(string id, string text) : base(id, text, "close")
         {
         }
 
-        public override void Add(EasyUITreeNode node)
+        public override void Add(EasyUITree node)
         {
             throw new NotImplementedException();
         }
 
-        public override void Remove(EasyUITreeNode node)
+        public override void Remove(EasyUITree node)
         {
             throw new NotImplementedException();
         }
+    }
+
+
+    public class CompositeClient
+    {
+        /// <summary>
+        /// 生成树
+        /// </summary>
+        /// <param name="node">节点（有子节点）</param>
+        /// <param name="lstChildren">子节点</param>
+        /// <param name="lstSearch">搜索范围（已排除子节点）</param>
+        public void GenTree(EasyUITreeNode node,List<NodeEntity> lstChildren, List<NodeEntity> lstSearch)
+        {
+            foreach (var n in lstChildren)
+            {
+                var lst = lstSearch.FindAll(x=>x.ParentId==n.Id);
+                if(lst.Count>0)
+                {
+                    var composite = new EasyUITreeNode(n.Id, n.Text, "open");
+                    node.Add(composite);                    
+                    lstSearch.RemoveAll(x => x.ParentId == n.Id);
+                    GenTree(composite, lst, lstSearch);
+                }
+                else
+                {
+                    var leaf = new EasyUITreeLeaf(n.Id,n.Text);
+                    node.Add(leaf);
+                }
+            }
+        }
+    }
+
+
+    public class NodeEntity
+    {
+        public string Id { get; set; }
+        public string ParentId { get; set; }
+        public string Text { get; set; }
     }
 
 }
