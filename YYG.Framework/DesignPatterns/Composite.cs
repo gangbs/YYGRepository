0ffa4cbfbf0d0e51711ad2d9f7e3ba34f87caf88
gangbs <yangyangangil@163.com>
2018-03-12 23:23:20 +0800

    [feat]组合模式

---
+++ YYG.Framework/DesignPatterns/Composite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YYG.Framework
+{
+    /// <summary>
+    /// easyui中的树节点
+    /// </summary>
+    public abstract class EasyUITree
+    {
+        public EasyUITree(string id,string text,string state)
+        {
+            this.id = id;
+            this.text = text;
+            this.state = state;
+        }
+
+
+        private string id { get; set; }
+        private string text { get; set; }
+        private string state { get; set; }
+        //public string checked { get; set; }
+
+        public abstract void Add(EasyUITreeNode node);
+        public abstract void Remove(EasyUITreeNode node);
+    }
+
+    public class EasyUITreeNode : EasyUITree
+    {
+        public EasyUITreeNode(string id, string text, string state):base(id,text, state)
+        {
+        }
+
+        public List<EasyUITreeNode> children = new List<EasyUITreeNode>();
+
+        public override void Add(EasyUITreeNode node)
+        {
+            children.Add(node);
+        }
+
+        public override void Remove(EasyUITreeNode node)
+        {
+            children.Remove(node);
+        }
+    }
+
+    public class EasyUITreeLeaf : EasyUITree
+    {
+        public EasyUITreeLeaf(string id, string text, string state) : base(id, text, state)
+        {
+        }
+
+        public override void Add(EasyUITreeNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove(EasyUITreeNode node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
