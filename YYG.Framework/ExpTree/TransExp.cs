11cad01126cb36f47b02128b1d24bb3eccc1d758
gangbs <yangyangangil@163.com>
2018-04-15 23:42:57 +0800

    表达式树赋值

---
+++ YYG.Framework/ExpTree/TransExp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YYG.Framework
+{
+   public class TransExpV1
+    {
+        private static Dictionary<string, object> _Dic = new Dictionary<string, object>();
+        private static TOut TransExp<TIn, TOut>(TIn tIn)
+        {
+            string key = string.Format("trans_exp_{0}_{1}", typeof(TIn).FullName, typeof(TOut).FullName);
+            if (!_Dic.ContainsKey(key))
+            {
+                ParameterExpression parameterExpression = Expression.Parameter(typeof(TIn), "p");
+                List<MemberBinding> memberBindingList = new List<MemberBinding>();
+
+                foreach (var item in typeof(TOut).GetProperties())
+                {
+
+                    if (!item.CanWrite)
+                        continue;
+                    MemberExpression property = Expression.Property(parameterExpression, typeof(TIn).GetProperty(item.Name));
+                    MemberBinding memberBinding = Expression.Bind(item, property);
+                    memberBindingList.Add(memberBinding);
+                }
+
+                MemberInitExpression memberInitExpression = Expression.MemberInit(Expression.New(typeof(TOut)), memberBindingList.ToArray());
+                Expression<Func<TIn, TOut>> lambda = Expression.Lambda<Func<TIn, TOut>>(memberInitExpression, new ParameterExpression[] { parameterExpression });
+                Func<TIn, TOut> func = lambda.Compile();
+
+                _Dic[key] = func;
+            }
+            return ((Func<TIn, TOut>)_Dic[key])(tIn);
+        }
+    }
+
+
+    public class TransExpV2<TIn, TOut>
+    {
+        private static readonly Func<TIn, TOut> cache = GetFunc();
+        private static Func<TIn, TOut> GetFunc()
+        {
+            ParameterExpression parameterExpression = Expression.Parameter(typeof(TIn), "p");
+            List<MemberBinding> memberBindingList = new List<MemberBinding>();
+            foreach (var item in typeof(TOut).GetProperties())
+            {
+                if (!item.CanWrite)
+                    continue;
+                MemberExpression property = Expression.Property(parameterExpression, typeof(TIn).GetProperty(item.Name));
+                MemberBinding memberBinding = Expression.Bind(item, property);
+                memberBindingList.Add(memberBinding);
+            }
+
+            MemberInitExpression memberInitExpression = Expression.MemberInit(Expression.New(typeof(TOut)), memberBindingList.ToArray());
+            Expression<Func<TIn, TOut>> lambda = Expression.Lambda<Func<TIn, TOut>>(memberInitExpression, new ParameterExpression[] { parameterExpression });
+            return lambda.Compile();
+        }
+
+        public static TOut Trans(TIn tIn)
+        {
+            return cache(tIn);
+        }
+    }
+}
