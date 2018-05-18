f2410d40932adcf9a1267187d0f1c5b9980b5d0d
杨彦钢 <yangyg@tendency.hz>
2018-05-18 09:38:43 +0800

    添加装饰者模式和代理模式

---
+++ YYG.Framework/DesignPatterns/DecoratorPattern/DecoratorPattern.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YYG.Framework.DesignPatterns.DecoratorPattern
+{
+    /// <summary>
+    /// 抽象主题
+    /// </summary>
+    public abstract class AbsSubject
+    {
+        public abstract void DoSomething();
+    }
+
+    /// <summary>
+    /// 具体主题，即被装饰的类
+    /// </summary>
+    public class ConSubject: AbsSubject
+    {
+        public override void DoSomething()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 抽象装饰者，完全代替了被装饰者的功能
+    /// </summary>
+    public abstract class AbsDecorator: AbsSubject
+    {
+        readonly AbsSubject obj;
+        public AbsDecorator(AbsSubject obj)
+        {
+            this.obj = obj;
+        }
+
+        public override void DoSomething()
+        {
+            if(obj!=null)
+            {
+                this.obj.DoSomething();
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 具体装饰者
+    /// </summary>
+    public class ConDecorator : AbsDecorator
+    {
+        public ConDecorator(AbsSubject obj) : base(obj) { }
+
+
+        public override void DoSomething()
+        {
+            base.DoSomething();//执行obj的dosomething
+            DoSomethingOther();//执行当前装饰者类具体的装饰
+        }
+
+        public void DoSomethingOther()
+        {
+
+        }
+    }
+
+
+    public class DecoratorClient
+    {
+        public void ClientOperation()
+        {
+
+        }
+    }
+}
---
+++ YYG.Framework/DesignPatterns/ProxyPattern/ProxyPattern.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YYG.Framework.DesignPatterns.ProxyPattern
+{
+    /// <summary>
+    /// 抽象主题，定义了真实主题和代理的规范
+    /// </summary>
+    public abstract class AbsSubject
+    {
+        public abstract void Operation();
+    }
+
+    /// <summary>
+    /// 真实主题
+    /// </summary>
+    public class ConSubject : AbsSubject
+    {
+        public override void Operation()
+        {
+            //doSomething
+        }
+    }
+
+    /// <summary>
+    /// 代理类，保持对真实主题的引用
+    /// </summary>
+    public class Proxy: AbsSubject
+    {
+        readonly ConSubject obj;
+
+        public Proxy()
+        {
+            this.obj = new ConSubject();//真实主题是在代理类内部生成，不同于装饰者模式（由外部传入，如构造函数注入）
+        }
+
+        public override void Operation()
+        {
+            this.BeforeOperation();
+            this.obj.Operation();
+            this.AfterOperation();
+        }
+
+        public void BeforeOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AfterOperation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
